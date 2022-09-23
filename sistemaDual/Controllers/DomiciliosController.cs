e46ea9f9d44e60a066822ff27acc15e771830851
neutroREM <dhamarloka90@gmail.com>
2022-09-23 03:56:08 -0500

    AlumDomicilioControllers

---
+++ sistemaDual/Controllers/DomiciliosController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sistemaDual.Data;
+using sistemaDual.Models;
+
+namespace sistemaDual.Controllers
+{
+    public class DomiciliosController : Controller
+    {
+        private readonly ProgramaDualContext _context;
+
+        public DomiciliosController(ProgramaDualContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Domicilios
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Domicilios.ToListAsync());
+        }
+
+        // GET: Domicilios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Domicilios == null)
+            {
+                return NotFound();
+            }
+
+            var domicilio = await _context.Domicilios
+                .Include(s => s.Universidades)
+                    .ThenInclude(e => e.ProgramaEducativos)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (domicilio == null)
+            {
+                return NotFound();
+            }
+
+            return View(domicilio);
+        }
+
+        // GET: Domicilios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Domicilios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Direccion,Colonia,Municipio,CodigoPostal,Otros")] Domicilio domicilio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(domicilio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(domicilio);
+        }
+
+        // GET: Domicilios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Domicilios == null)
+            {
+                return NotFound();
+            }
+
+            var domicilio = await _context.Domicilios.FindAsync(id);
+            if (domicilio == null)
+            {
+                return NotFound();
+            }
+            return View(domicilio);
+        }
+
+        // POST: Domicilios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Direccion,Colonia,Municipio,CodigoPostal,Otros")] Domicilio domicilio)
+        {
+            if (id != domicilio.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(domicilio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DomicilioExists(domicilio.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(domicilio);
+        }
+
+        // GET: Domicilios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Domicilios == null)
+            {
+                return NotFound();
+            }
+
+            var domicilio = await _context.Domicilios
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (domicilio == null)
+            {
+                return NotFound();
+            }
+
+            return View(domicilio);
+        }
+
+        // POST: Domicilios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Domicilios == null)
+            {
+                return Problem("Entity set 'ProgramaDualContext.Domicilios'  is null.");
+            }
+            var domicilio = await _context.Domicilios.FindAsync(id);
+            if (domicilio != null)
+            {
+                _context.Domicilios.Remove(domicilio);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DomicilioExists(int id)
+        {
+          return _context.Domicilios.Any(e => e.ID == id);
+        }
+    }
+}
